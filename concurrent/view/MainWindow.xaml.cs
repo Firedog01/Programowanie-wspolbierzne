4b9f05ff689dddecba97f4e03b41b9163f5c0b4c
LiptonLeon <236496@gmail.com>
2022-04-16 19:08:07 +0200

    Viewmodel binding

--- concurrent/view/MainWindow.xaml.cs
+++ concurrent/view/MainWindow.xaml.cs
@@ -12,17 +12,33 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using viewModel;
 
 namespace view
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
+            AllocConsole();
+            DataContext = new ViewModel();
             InitializeComponent();
+        }
+
+        // Show console for debugging
+        [System.Runtime.InteropServices.DllImport("kernel32.dll")]
+        private static extern bool AllocConsole();
+
+        // Actual canvas size cannot be access using binding
+        private void canvas_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            var vm = DataContext as ViewModel;
+            vm.CanvasHeight = (float)e.NewSize.Height;
+            vm.CanvasWidth = (float)e.NewSize.Width;
         }
     }
 }
