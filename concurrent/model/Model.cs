5150fbd0a77abe1e2c0f60165680c4407c414c77
Firedog01 <jan.rubacha@onet.pl>
2022-04-20 13:24:46 +0200

    added model file

---
+++ concurrent/model/Model.cs
@@ -0,0 +1,37 @@
+﻿using logic;
+using logic.Event;
+
+namespace model
+{
+    public class Model
+    {
+        //singleton
+        private static Model instance = null;
+        private static readonly object padlock = new object();
+
+        //
+
+
+        private Model() 
+        {
+            Api.Instance.PosUpdated += new MarbleEventHandler(OnPosUpdated);
+        }
+
+        public static Model Instance
+        {
+            get
+            {
+                lock (padlock) // critical section
+                {
+                    if (instance == null)
+                        instance = new Model();
+                    return instance;
+                }
+            }
+        }
+
+        public void OnPosUpdated(object source, MarbleArgs args)
+        {
+        }
+    }
+}
