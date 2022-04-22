f7af98518a34e7c6988e0bd248da0bd8e87f7235
Firedog01 <jan.rubacha@onet.pl>
2022-04-22 09:50:03 +0200

    Uwaga! Nie byly dodane pliki. Trzeba patrzec tutaj

---
+++ concurrent/logic/LogicApi.cs
@@ -0,0 +1,47 @@
+﻿using System.Numerics;
+using logic.Event;
+
+namespace logic
+{
+    public abstract class LogicApi
+    {
+        // singleton
+        private static LogicApiImplementation implementation;
+        private static readonly object padlock = new object();
+
+        public static LogicApi Implementation
+        {
+            get
+            {
+                lock (padlock) // critical section
+                {
+                    if (implementation == null)
+                        implementation = new LogicApiImplementation();
+                    return implementation;
+                }
+            }
+        }
+
+        public abstract int MarbleCount { get; }
+        public abstract Vector2 CanvasSize { get; set; }
+        public abstract float NewMarbleRadius { get; set; }
+        public event MarbleEventHandler PosUpdated;
+
+        protected virtual void DoPosUpdated(MarbleInfo[] args)
+        {
+            // send updated position
+            if (PosUpdated != null)
+            {
+                PosUpdated(this, new MarbleArgs(args));
+            }
+        }
+
+        
+
+        public abstract void CreateUnmovingMarble();
+        public abstract void CreateMovingMarble();
+        public abstract void DeleteMarble(int idx);
+        public abstract void Start();
+        public abstract void Stop();
+    }
+}
---
+++ concurrent/test/LogicApiTest.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using logic;
+using logic.Event;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Diagnostics; // Process
+
+namespace test
+{
+    [TestClass]
+    public class LogicApiTest
+    {
+        private TextWriterTraceListener myListener;
+        private int threadWaitTime = 100;
+        public LogicApiTest()
+        {
+            // file located in concurrent\test\bin\Debug\net6.0\ApiTest.log
+            myListener = new TextWriterTraceListener("ApiTest.log", "myListener");
+            myListener.WriteLine("================ Beginning of test class at: " + DateTime.Now.ToString("MM-dd hh:mm:ss"));
+            myListener.Flush();
+            LogicApi.Implementation.PosUpdated += new MarbleEventHandler(PrintMarblesPos);
+        }
+
+        ~LogicApiTest()
+        {
+            myListener.Flush();
+        }
+
+        [TestMethod]
+        public void ApiListenerTest()
+        {
+            myListener.WriteLine("ApiListenerTest ------------------------");
+            LogicApi.Implementation.CreateUnmovingMarble();
+            myListener.WriteLine("created one marble.");
+            
+            myListener.WriteLine("added handler to event.");
+
+            myListener.WriteLine("starting to sleep on main thread.");
+            Thread.Sleep(threadWaitTime);
+
+            myListener.WriteLine("deleted one marble.");
+            LogicApi.Implementation.DeleteMarble(0);
+            myListener.WriteLine("end ApiListenerTest ------------------------");
+            myListener.Flush();
+        }
+
+        [TestMethod]
+        public void CreateDeleteTest()
+        {
+            myListener.WriteLine("CreateDeleteTest ------------------------");
+            myListener.Flush();
+            int a;
+
+            for (int i = 0; i < 3; i++)
+            {
+                a = LogicApi.Implementation.MarbleCount;
+                Assert.AreEqual(i, a);
+                LogicApi.Implementation.CreateUnmovingMarble();
+                Thread.Sleep(threadWaitTime);
+            }
+
+            for (int i = 0; i < 3; i++)
+                LogicApi.Implementation.DeleteMarble(0);
+            myListener.WriteLine("deleted three marbles.");
+            myListener.WriteLine("end CreateDeleteTest ------------------------");
+            myListener.Flush();
+        }
+
+        [TestMethod]
+        public void CreateMovingTest()
+        {
+            LogicApi.Implementation.CreateMovingMarble();
+            LogicApi.Implementation.Start();
+            Thread.Sleep(threadWaitTime);
+        }
+
+        
+
+        public void PrintMarblesPos(object source, MarbleArgs args)
+        {
+            myListener.WriteLine("PosUpdated:");
+            foreach(MarbleInfo arg in args.InfoArr)
+            {
+                string s = "x: " + arg.X;
+                s += ", y: " + arg.Y;
+                s += ", idx: " + arg.Index;
+                myListener.WriteLine(s);
+            }
+            myListener.Flush();
+        }
+    }
+}
